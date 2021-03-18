246664177d5f8544672a410f9802ec74c0ff543d
Ayush Suman <ayush231suman@gmail.com>
2021-03-18 14:37:45 +0530

    Crack code for Find the difference

---
+++ crack.py
@@ -0,0 +1,20 @@
+#pip3 install opencv-python
+import cv2
+
+#Enter image name of first image to compare
+imagename = ""
+
+#Enter image name of second image to compare
+imagename2 = ""
+img = cv2.imread(imagename)
+img2 = cv2.imread(imagename2)
+
+(length, breadth, color) = img.shape
+
+for i in range(length):
+    for j in range(breadth):
+        if img[i, j].all() != img2[i, j].all():
+            print(img2[i,j])
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
